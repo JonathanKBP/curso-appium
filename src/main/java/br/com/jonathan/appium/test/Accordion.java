0a958d07cdf5ba9266dd170fb60705c780ed2265
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2023-04-08 03:19:25 -0300

    Aula: Accordion

---
+++ src/main/java/br/com/jonathan/appium/test/Accordion.java
@@ -0,0 +1,24 @@
+package br.com.jonathan.appium.test;
+
+import org.junit.Test;
+
+import br.com.jonathan.appium.core.BaseTest;
+import br.com.jonathan.appium.page.AccordionPage;
+import br.com.jonathan.appium.page.MenuPage;
+import junit.framework.Assert;
+
+public class Accordion extends BaseTest {
+	
+	private MenuPage menu = new MenuPage();
+	private AccordionPage page = new AccordionPage();
+	
+	@Test
+	public void deveInteragirComAccordion() {
+		menu.acessarAccordion();
+		
+		page.selecionarOp1();
+		esperar(1000);
+		Assert.assertEquals("Esta é a descrição da opção 1", page.obterValorOp1());
+	}
+
+}
