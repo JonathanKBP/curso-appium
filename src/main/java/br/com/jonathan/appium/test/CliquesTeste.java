2b5f660b3d3c1607be987f551373afbf5c3a781b
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2023-04-10 23:29:51 -0300

    Aula: Clique duplo

--- src/main/java/br/com/jonathan/appium/test/CliquesTeste.java
+++ src/main/java/br/com/jonathan/appium/test/CliquesTeste.java
@@ -1,5 +1,6 @@
 package br.com.jonathan.appium.test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.jonathan.appium.core.BaseTest;
@@ -11,13 +12,24 @@ public class CliquesTeste extends BaseTest {
 	
 	private MenuPage menu = new MenuPage();
 	private CliquesPage page = new CliquesPage();
+	
+	@Before
+	public void setup() {
+		menu.acessarCliques();
+	}
 
 	@Test
 	public void deveRealizarCliqueLongo() {
-		menu.acessarCliques();
-		
 		page.cliqueLongo();
 		
 		Assert.assertEquals("Clique Longo", page.obterTextoCampo());
+	}
+	
+	@Test
+	public void deveRealizarCliqueDuplo() {
+		page.clicarPorTexto("Clique duplo");
+		page.clicarPorTexto("Clique duplo");
+		
+		Assert.assertEquals("Duplo Clique", page.obterTextoCampo());
 	}
 }
