96bd3bc0e9b9a76a6ef0ede173605d6fd6920271
Jonathan Pinto <jonathan.kevinbp@hotmail.com>
2023-04-11 00:37:40 -0300

    Aula: Scroll

--- src/main/java/br/com/jonathan/appium/core/BasePage.java
+++ src/main/java/br/com/jonathan/appium/core/BasePage.java
@@ -5,6 +5,7 @@ import static br.com.jonathan.appium.core.DriverFactory.getDriver;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
@@ -42,7 +43,26 @@ public class BasePage {
 		 return elementos.size() > 0;
 	}
 	
+	public String obterTituloAlerta() {
+		return obterTexto(By.id("android:id/alertTitle"));
+	}
+	
+	public String obterMensagemAlerta() {
+		return obterTexto(By.id("android:id/message"));
+	}
+	
 	public void tap(int x, int y) {
 		new TouchAction(getDriver()).tap(PointOption.point(x, y)).perform();
+	}
+	
+	public void scroll (double inicio, double fim) {
+		Dimension size = getDriver().manage().window().getSize();
+		
+		int x = size.width / 2;
+		
+		int start_y = (int) (size.height * inicio);
+		int end_y = (int) (size.height * fim);
+		
+		new TouchAction(getDriver()).longPress(PointOption.point(x, start_y)).moveTo(PointOption.point(x, end_y)).release().perform();
 	}
 }
---
+++ src/main/java/br/com/jonathan/appium/test/OpcaoEscondidaTeste.java
@@ -0,0 +1,32 @@
+package br.com.jonathan.appium.test;
+
+import static br.com.jonathan.appium.core.DriverFactory.getDriver;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import br.com.jonathan.appium.core.BaseTest;
+import br.com.jonathan.appium.core.DriverFactory;
+import br.com.jonathan.appium.page.MenuPage;
+import junit.framework.Assert;
+
+public class OpcaoEscondidaTeste extends BaseTest {
+	
+	private MenuPage menu = new MenuPage();
+
+	@Test
+	public void deveEncontrarOpcaoEscondida() {
+		WebDriverWait wait = new WebDriverWait(getDriver(), 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Formulário']")));
+		menu.scroll(0.9, 0.1);
+		
+		menu.clicarPorTexto("Opção bem escondida");
+		
+		Assert.assertEquals("Você achou essa opção", menu.obterMensagemAlerta());
+		
+		menu.clicarPorTexto("OK");
+	}
+}
